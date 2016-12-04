b4d5f696291b042bc93997f5023b2357730e26c9
Soramichi Akiyama <akiyama@m.soramichi.jp>
2016-12-04 09:43:06 +0900

    delete garbage

--- fft.py
+++ fft.py
@@ -4,11 +4,6 @@ import wave
 import pickle
 import functools
 import numpy as np
-"""
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pylab as plt
-"""
 
 base_dir = sys.argv[1]
 dir = base_dir + "/cut"
@@ -32,11 +27,3 @@ for i in range(0, len(filelist)):
     # save only up to 15kHz
     # Note: one Hz has two data elements (real and imaginary parts)
     sys.stdout.write("%s, %s\n" % (dir + "/" + f, list(data[0:30000]).__str__()[1:-1]))
-
-"""
-plt.plot(freqList, amplitudeSpectrum, marker= 'o', linestyle='-')
-plt.axis([0, 1600, 0, 250])
-plt.xlabel("frequency [Hz]")
-plt.ylabel("amplitude spectrum")
-plt.savefig("test.png")
-"""
